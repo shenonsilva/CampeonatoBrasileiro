6d67128320bbfc1836a581393d5d749e4fc9648e
Shenon Oliveira e Silva <shenonsilva@gmail.com>
2022-03-18 20:11:09 -0300

    Novo Campeonato

---
+++ campeonato/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <TelaUm/>
+    <v-main>
+
+    </v-main>    
+  </v-app>
+</template>
+
+<script>
+import TelaUm from './components/TelaUm.vue'
+export default {
+  name: 'campBraza',
+  components: {
+    TelaUm
+  }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ campeonato/src/components/TelaUm.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'campBraza',
+  data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
